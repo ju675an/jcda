211308b010f6b26944a872305ee4449d79d94452
ju675an <125043958+ju675an@users.noreply.github.com>
2023-05-30 22:14:27 -0500

    Add files via upload

---
+++ hmtl code/slideshow.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>My Title</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style>
+</style>
+<script src="https://www.w3schools.com/lib/w3.js"></script>
+<body>
+
+<img class="images" src="cocora.jpeg" width="50%">
+<img class="images" src="monkey.jpeg" width="50%">
+<img class="images" src="parchado.jpeg" width="50%">
+
+<script>
+w3.slideshow(".nature", 1500);
+</script>
+
+</body>
+</html>
